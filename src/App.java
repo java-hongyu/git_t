a8428a43db44b50074135d65c293f590f3fcfda0
hongyu <2100627601@qq.com>
2022-11-01 13:43:53 +0800

    update App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App{
 	public static void main(String[] args){
-		System.out.println("hello world!");
+		int count = 140;
+		float a = 1.0f*count/20;
+		if(a - (int)a > 0){
+			a += 1;
+		}
+		System.out.println((int)a);
 	}
 }
